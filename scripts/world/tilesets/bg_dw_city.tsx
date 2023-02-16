5a4bb4948a3cb66ea53221d846869e0ab6c2a991
lexio <lexio2121@gmail.com>
2023-02-16 15:17:05 -0500

    Add files via upload

---
+++ scripts/world/tilesets/bg_dw_city.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="bg_dw_city" tilewidth="40" tileheight="40" tilecount="612" columns="17">
+ <image source="../../../assets/sprites/tilesets/bg_dw_city.png" width="680" height="1440"/>
+</tileset>
